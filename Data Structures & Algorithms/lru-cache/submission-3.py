b9845ee22af72d60a21984d9b369f86218c38b96
Daniyal Usman <uzairusman0322@gmail.com>
2026-08-10 18:58:14 +0500

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self,key=0,val=0):
+        self.key=key
+        self.val=val
+        self.prev=None
+        self.next=None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity=capacity
+        self.cache={}
+        self.head=Node()
+        self.tail=Node()
+        self.head.next=self.tail
+        self.tail.prev=self.head
+        
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node=self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.val
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node=self.cache[key]
+            node.val=value
+            self._remove(node)
+            self._add(node)
+        else:
+            if len(self.cache)>=self.capacity:
+                lru=self.tail.prev
+                self._remove(lru)
+                del self.cache[lru.key]
+            new_node=Node(key,value)
+            self.cache[key]=new_node
+            self._add(new_node)
+    def _add(self,node):
+        node.prev=self.head
+        node.next=self.head.next
+        self.head.next.prev=node
+        self.head.next=node
+    def _remove(self,node):
+        node.prev.next=node.next
+        node.next.prev=node.prev
+
+        
