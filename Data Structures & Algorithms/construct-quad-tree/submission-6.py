f362b469f7a1e64d451e128fbc3a04879b67afd7
Daniyal Usman <daniyalusman.dev@gmail.com>
2026-08-21 22:08:31 +0500

    Add: construct-quad-tree - submission-6

---
+++ Data Structures & Algorithms/construct-quad-tree/submission-6.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val=False, isLeaf=False, topLeft=None, topRight=None, bottomLeft=None, bottomRight=None):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        def dfs(row_start,row_end,col_start,col_end):
+            first_val=grid[row_start][col_start]
+            is_uniform=True
+            for i in range(row_start,row_end):
+                for j in range(col_start,col_end):
+                    if grid[i][j]!=first_val:
+                        is_uniform=False
+                        break
+                if not is_uniform:
+                    break
+            if is_uniform:
+                return Node(first_val==1,True,None,None,None,None)
+            row_mid=(row_start+row_end)//2
+            col_mid=(col_start+col_end)//2
+            topLeft=dfs(row_start,row_mid,col_start,col_mid)
+            topRight=dfs(row_start,row_mid,col_mid,col_end)
+            bottomLeft=dfs(row_mid,row_end,col_start,col_mid)
+            bottomRight=dfs(row_mid,row_end,col_mid,col_end)
+            return Node(True,False,topLeft,topRight,bottomLeft,bottomRight)
+        n=len(grid)
+        return dfs(0,n,0,n)
